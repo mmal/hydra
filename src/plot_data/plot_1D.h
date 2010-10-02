10b0dc48b9a14fbf5882deb5196b539319a4a279
Maciej Maliborski <maciej.maliborski@gmail.com>
2010-10-02 10:44:30 +0200

    structure modification 10

---
+++ src/plot_data/plot_1D.h
@@ -0,0 +1,34 @@
+#ifndef _PLOT_1D_H_
+#define _PLOT_1D_H_
+
+
+#include <stdio.h>
+#include <string.h>
+
+
+#include "src/common/types.h"
+#include "src/common/grids_common.h"
+#include "src/common/amrp_common.h"
+#include "src/stat_log/stat_msg.h"
+#include "src/plot_data/gnuplot_i.h"
+
+
+void _h_1D_set_plot_options ( gnuplot_ctrl * h, const char * title,
+                              const H_DBL time );
+
+void _h_1D_add_plot_options ( gnuplot_ctrl * h, const char *option );
+
+void _h_1D_plot_set_of_grids_recursion ( h_grid * g,
+                                         gnuplot_ctrl * handler );
+
+
+void h_1D_plot_one_grid ( h_grid * g, int rank, int wghosts,
+                          const char* title, int sleep_time );
+
+void h_1D_plot_set_of_grids ( h_grid * cg, int lmax, const char* title,
+                              int sleep_time );
+
+void h_1D_plot_set_of_grids_2 ( h_grid * cg, int rank, int wghosts,
+                                const char* title, int sleep_time );
+
+#endif /* _PLOT_1D_H_ */
